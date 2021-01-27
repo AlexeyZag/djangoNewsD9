605b3677f49cecb851f2edc0d444d7357994e772
AlexeyZag <72604307+AlexeyZag@users.noreply.github.com>
2021-01-27 12:32:49 +0300

    Update settings.py

--- djangoNews1/settings.py
+++ djangoNews1/settings.py
@@ -162,7 +162,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 EMAIL_HOST = 'smtp.yandex.ru' # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465 # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'zagaalexey' # ваше имя пользователя, например если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'yety.2000.alex' # пароль от почты
+EMAIL_HOST_PASSWORD = '' # пароль от почты
 EMAIL_USE_SSL = True # Яндекс использует ssl, подробнее о том, что это, почитайте на Википедии, но включать его здесь обязательно
 ADMINS = [
     ('alex8.8', 'alex8.8@mail.ru'),
